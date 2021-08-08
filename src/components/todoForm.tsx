12b943399a58b308aa769a9b4a8fddca62dc87bd
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-08-08 14:08:42 -0500

    useRef para textField

--- src/components/todoForm.tsx
+++ src/components/todoForm.tsx
@@ -1,23 +1,45 @@
 import * as React from "react";
-import { PrimaryButton, TextField, Stack } from "@fluentui/react";
+import { PrimaryButton, TextField, Stack, ITextField } from "@fluentui/react";
+import { addTodo } from "../actions/todo";
 
 export default function TodoForm() {
-  const [title, setTitle] = React.useState<string>();
+  const [title, setTitle] = React.useState<string>("");
+  let titleRef = React.useRef<ITextField>();
 
   function handleChangeTitle(
-    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
-    newValue?: string
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
   ) {
     setTitle((event.target as HTMLInputElement).value);
+  }
+
+  const setTitleFieldRef = (textField: ITextField) => {
+    titleRef.current = textField;
+  };
+
+  function handleAddTodo(e) {
+    e.preventDefault();
+    if (title === "") return;
+    addTodo(title);
+    setTitle("");
+
+    if (titleRef.current) {
+      (titleRef.current as ITextField).focus();
+    }
   }
 
   return (
     <Stack horizontal>
       <Stack.Item grow>
-        <TextField onChange={handleChangeTitle} />
+        <form onSubmit={handleAddTodo}>
+          <TextField
+            componentRef={setTitleFieldRef}
+            onChange={handleChangeTitle}
+            value={title}
+          />
+        </form>
       </Stack.Item>
       <Stack.Item>
-        <PrimaryButton text="Add Todo" value={title} />
+        <PrimaryButton text="Add Todo" onClick={handleAddTodo} />
       </Stack.Item>
     </Stack>
   );
